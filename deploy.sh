7e546fd6c9951a0b780ea1103971063e8be22e36
root <root@ip-172-31-28-67.us-west-2.compute.internal>
2023-12-23 07:32:15 +0000

    deploy file is modified

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 #set the image
 IMAGENAME="my-reactapp"
-branch= returnStdout: true , script: git rev-parse --abbrev-ref HEAD
-if [ '$branch' == 'dev' ] ; then
+ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+ #If  the branch is dev, build the image and push it to docker-hub dev repository
+if [ "$BRANCH" == "dev" ]; then
+    echo "Building image for dev branch "
     docker build -t $IMAGENAME .
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
-    docker push /dev:latest
+    docker tag my-reactapp sanjusanjay23/dev
+    docker push sanjusanjay23/dev:latest
     docker logout
+
+elif [ "$BRANCH" == "prod" ]; then
+      echo "Building image for prod branch ..."
+      docker build -t $IMAGENAME .
+      echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
+      docker tag my-reactapp sanjusanjay23/prod
+      docker push sanjusanjay23/prod:latest
+      docker logout
+
 fi
+
